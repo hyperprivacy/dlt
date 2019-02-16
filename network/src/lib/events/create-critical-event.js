580cc047d070f7b61575364acb0f17c4284cb2fc
Glibo Kristijan <glibo.kristijan@gmail.com>
2019-02-16 22:38:35 +0100

    Adding critical events

---
+++ network/src/lib/events/create-critical-event.js
@@ -0,0 +1,27 @@
+'use strict';
+
+require('../../../../config');
+const uuidV1 = require('uuid/v1');
+var invoke = require('../chaincodes/invoke-chaincode');
+
+
+var transient = {
+    sensor_id: uuidV1(),
+    data: {
+        door_opened: true,
+        lat: '48.775845',
+        lng: '9.182932'
+    }
+};
+
+invoke.invokeChaincode([
+    'peer0.enduser.hyperprivacy.network.com'], 
+    'eventchannel',
+    'events',
+    'createCriticalEvent',
+    [uuidV1()],
+    'EndUser',
+    'enduser_admin',
+    transient).then((res) => {
+        console.log(res);
+    });
